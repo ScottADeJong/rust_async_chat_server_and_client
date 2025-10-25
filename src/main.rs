9e409e55ae12ac68f1fa60b03986d0ee08663989
ScottADeJong <38508855+ScottADeJong@users.noreply.github.com>
2025-10-25 12:25:51 -0400

    Restructured command processessing and error handling

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,40 @@ fn sleep() {
     thread::sleep(::std::time::Duration::from_millis(SLEEP));
 }
 
+struct User {
+    nick_name: String,
+    is_active: bool,
+}
+
+impl User {
+    fn new(nick_name: String) -> Self {
+        Self {
+            nick_name,
+            is_active: true,
+        }
+    }
+
+    fn disconnect(&mut self) {
+        self.is_active = false;
+    }
+}
+
+fn process_command(command: &str, user: &mut User) {
+    let command: Vec<&str> = command.split_whitespace().collect();
+    if let Some(c) = command.first() {
+        match *c {
+            ":quit" => user.disconnect(),
+            ":name" => {
+                user.nick_name = match command.get(1) {
+                    Some(n) => n.to_string(),
+                    None => String::from("unknown"),
+                }
+            }
+            _ => (),
+        }
+    }
+}
+
 fn main() {
     let server = TcpListener::bind(LOCAL).expect("Listener failed to bind");
     server
@@ -27,18 +61,32 @@ fn main() {
             clients.push(socket.try_clone().expect("failed to clone client"));
 
             thread::spawn(move || {
+                let mut user = User::new(addr.to_string());
                 loop {
                     let mut buff = vec![0; MSG_SIZE];
+                    if !user.is_active {
+                        println!("got here");
+                        socket.shutdown(std::net::Shutdown::Both).unwrap();
+                    }
 
-                    match socket.read_exact(&mut buff) {
+                    match socket.read(&mut buff) {
                         Ok(_) => {
-                            let msg = buff.into_iter().take_while(|&x| x != 0).collect::<Vec<_>>();
-                            let msg = String::from_utf8(msg).expect("Invalid utf8 message");
-
-                            let msg = format!("{}: {}", addr, msg);
-                            let msg = msg.replace('"', "");
-                            tx.send(msg).expect("failed to send msg to rx");
+                            let msg =
+                                String::from_utf8(buff.into_iter().filter(|n| *n != 0).collect())
+                                    .expect("Invalid utf8 message");
+                            match msg.trim() {
+                                "" => (),
+                                n if n.starts_with(":") => process_command(n, &mut user),
+                                _ => {
+                                    let msg = format!("{}: {}", user.nick_name, msg);
+                                    let msg = msg.replace('"', "");
+                                    tx.send(msg).expect("failed to send msg to rx");
+                                }
+                            }
                         }
+                        // This error is ignored because it's really just a real-time
+                        // warning saying that nothing is available at the moement, so
+                        // skipping this polling attempt.
                         Err(ref err) if err.kind() == ErrorKind::WouldBlock => (),
                         Err(_) => {
                             println!("closing connection with: {}", addr);
