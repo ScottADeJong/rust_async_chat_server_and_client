f58c5a240da36b958a3b5149ef6e366431e7ef41
ScottADeJong <38508855+ScottADeJong@users.noreply.github.com>
2025-10-25 12:25:24 -0400

    Restructured nicknames and command handling.

--- src/main.rs
+++ src/main.rs
@@ -8,31 +8,64 @@ const LOCAL: &str = "127.0.0.1:6000";
 const MSG_SIZE: usize = 255;
 const SLEEP: u64 = 100;
 
+struct Message {
+    source: String,
+    message: String,
+}
+
+#[derive(Clone)]
+struct User {
+    nick_name: String,
+    is_active: bool,
+}
+
+impl User {
+    fn new(nick_name: String) -> Self {
+        Self {
+            nick_name,
+            is_active: true,
+        }
+    }
+
+    fn disconnect(&mut self) {
+        self.is_active = false;
+    }
+}
+
 fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    let mut user = match args.get(1) {
+        Some(s) => User::new(s.to_string()),
+        None => {
+            eprintln!("usage: {} user_name", args[0]);
+            return;
+        }
+    };
+
     let mut client = TcpStream::connect(LOCAL).expect("Stream failed to connect");
     client
         .set_nonblocking(true)
         .expect("failed to initiate non-blocking");
 
-    let local_addr = client.local_addr().unwrap().to_string();
     let (tx, rx) = mpsc::channel::<String>();
 
+    let thread_user = user.clone();
     thread::spawn(move || {
         loop {
             let mut buff = vec![0; MSG_SIZE];
-            match client.read_exact(&mut buff) {
+            match client.read(&mut buff) {
                 Ok(_) => {
-                    let msg: String = buff
-                        .into_iter()
-                        .take_while(|&x| x != 0)
-                        .map(|n| n.into())
-                        .collect::<Vec<char>>()
-                        .into_iter()
-                        .collect();
-                    if !msg.contains(local_addr.as_str()) {
-                        println!("{:?}", msg);
+                    let msg = String::from_utf8(buff.into_iter().filter(|n| *n != 0).collect())
+                        .expect("Invalid utf8 message");
+                    if !msg.is_empty()
+                        && !msg.contains(format!("{}: ", &thread_user.nick_name).as_str())
+                    {
+                        println!("--->{:?}", msg);
                     }
                 }
+                // This error is ignored because it's really just a real-time
+                // warning saying that nothing is available at the moement, so
+                // skipping this polling attempt.
                 Err(ref err) if err.kind() == ErrorKind::WouldBlock => (),
                 Err(_) => {
                     println!("connection with server was severed");
@@ -55,12 +88,17 @@ fn main() {
     });
 
     println!("Write a Message:");
+    if tx.send(format!(":name {}", user.nick_name)).is_err() {
+        return;
+    };
+
     loop {
         let mut buff = String::new();
         io::stdin()
             .read_line(&mut buff)
             .expect("reading from stdin failed");
         let msg = buff.trim().to_string();
+
         if msg == ":quit" || tx.send(msg).is_err() {
             break;
         }
